75b9a5911fe5f1ac28813c01aa5d7f1fed2ed590
Taki <Al_kuduki_82@mail.ru>
2022-11-30 16:25:15 +0300

    Changing the library connection to work with json

--- SearchEngine/main.cpp
+++ SearchEngine/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
 
-using namespace std;
+using json = nlohmann::json;
+using std::cin;
+using std::cout;
+using std::endl;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    std::ifstream f("../example.json");
+    json data;
+    if(f.is_open())
+    {
+        data = json::parse(f);
+        f.close();
+
+    }
+    else std::cerr << "file not fond" << endl;
+
+    if(!data.empty())
+    {
+//        cout << data.dump(4) << endl;
+        for (auto& el : data.items())
+        {
+            std::cout << "key: " << el.key() << ", value: " << el.value() << '\n';
+        }
+    }
+
     return 0;
 }
+
+
